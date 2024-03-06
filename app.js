f98d6e6ed2fb4656267328db487a69f91589f8b1
Anananastejsi <154801682+Anananastejsi@users.noreply.github.com>
2024-03-06 19:33:20 +0400

    Update app.js

--- app.js
+++ app.js
@@ -56,28 +56,3 @@ btn4.addEventListener("click", function () {
 Telegram.WebApp.onEvent("mainButtonClicked", function () {
     tg.sendData(item);
 });
-
-
-
-web_app=WebAppInfo(url="https://anananastejsi.github.io/first/")
-
-keyboard = ReplyKeyboardMarkup(
-    keyboard=[
-        [KeyboardButton(text="Языки",web_app=web_app)]
-    ],
-    resize_keyboard=True
-)
-
-
-DISC = {
-    '1': 'nice!!!',
-    '2': 'good!!!',
-    '3': 'awesome!!!',
-    '4': 'hehehe'
-}
-
-
-@dp.message_handler(content_types='web_app_data')
-async def buy_process(web_app_message):
-    await bot.send_message(web_app_message.chat.id,
-DISC[f'{web_app_message.web_app_data}'])
